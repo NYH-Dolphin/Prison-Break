41e620c784b1e772f01775f6df87e9b54b639b13
小小的大肥 <11911839@mail.sustech.edu.cn>
2023-11-02 23:42:58 -0400

    weapon detection

---
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Weapon;
+
+namespace Player
+{
+    public class PlayerWeapon : MonoBehaviour
+    {
+        [SerializeField] private float fWeaponGrabRange;
+        [SerializeField] private LayerMask lmWeapon;
+
+        private GameObject _weaponSelected;
+
+
+        private void Update()
+        {
+            WeaponDetectionUpdate();
+        }
+
+
+        private void WeaponDetectionUpdate()
+        {
+            Collider[] hitColliders = new Collider[1];
+            int num = Physics.OverlapSphereNonAlloc(transform.position, fWeaponGrabRange, hitColliders, lmWeapon);
+            if (num != 0)
+            {
+                GameObject weapon = hitColliders[0].gameObject;
+                if (_weaponSelected != weapon)
+                {
+                    weapon.GetComponent<WeaponBehaviour>().OnSelected();
+                    if (_weaponSelected != null) _weaponSelected.GetComponent<WeaponBehaviour>().OnNotSelected();
+                    _weaponSelected = weapon;
+                }
+            }
+            else
+            {
+                if (_weaponSelected != null)
+                {
+                    _weaponSelected.GetComponent<WeaponBehaviour>().OnNotSelected();
+                    _weaponSelected = null;
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, fWeaponGrabRange);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon/WeaponBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Weapon
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class WeaponBehaviour : MonoBehaviour
+    {
+        private SpriteRenderer _sr;
+        private Material _mat; // require material "2d Sprite Glow"
+        private static readonly int OutlineWidth = Shader.PropertyToID("_OutlineWidth");
+
+        private void Awake()
+        {
+            _sr = GetComponent<SpriteRenderer>();
+            _mat = _sr.material;
+            OnNotSelected();
+        }
+
+        /// <summary>
+        /// Weapon is selected and ready for grabbing
+        /// </summary>
+        public void OnSelected()
+        {
+            _mat.SetFloat(OutlineWidth, 5);
+        }
+
+        public void OnNotSelected()
+        {
+            _mat.SetFloat(OutlineWidth, 0);
+        }
+    }
+}
