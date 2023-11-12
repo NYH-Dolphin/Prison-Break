36d0fb8bcda308c765a9cecfba2470d893e006d6
小小的大肥 <11911839@mail.sustech.edu.cn>
2023-11-12 17:53:21 -0500

    throw behaviour

--- Assets/Scripts/Weapon/ThrowWeaponBehaviour.cs
+++ Assets/Scripts/Weapon/ThrowWeaponBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Weapon
@@ -7,20 +8,58 @@ namespace Weapon
     {
         [SerializeField] private float fThrowForce = 20f;
         [SerializeField] private float fThrowTime = 3f;
-
+        [SerializeField] private LayerMask lmGround;
+        
+        private Vector3 _vecThrowDir;
+        
+        private void Update()
+        {
+            // Player Hold the Weapon, start detection
+            if (Pw != null)
+            {
+                if (!bAttack)
+                {
+                    if (Camera.main != null)
+                    {
+                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit, Mathf.Infinity, lmGround))
+                        {
+                            // The ray hit an object on the ground layer
+                            Vector3 hitGround = hit.point;
+                            hitGround.y = 0f;
+                            Vector3 playerPos = Pc.transform.position;
+                            playerPos.y = 0f;
+                            _vecThrowDir = (hitGround - playerPos).normalized;
+                            Pw.OnDrawWeaponDir(_vecThrowDir);
+                        }
+                    }
+                }
+                else
+                {
+                    Pw.OnCancelDrawWeaponDir();
+                }
+            }
+        }
+        
         public override void OnAttack()
         {
-            ThrowBehaviour(Pw.GetPlayerVecDir());
+            ThrowBehaviour(_vecThrowDir);
         }
+        
 
         private void ThrowBehaviour(Vector3 facingDir)
         {
             bAttack = true;
+            
+            Pc.OnSetAttackDir(_vecThrowDir);
+            Pc.OnAttackPerformed(weaponInfo.eAttackType);
+            
             Rb.drag = 0f;
             Rb.angularDrag = 0f;
             Rb.constraints = RigidbodyConstraints.FreezePositionY;
             Rb.AddForce(facingDir * fThrowForce, ForceMode.Impulse);
-            Pc.OnAttackPerformed(weaponInfo.eAttackType);
+            
             StartCoroutine(DestroyCountDown(fThrowTime));
         }
 
