caa787e6ec0cff6f2e67088dc5a564cb965dc4f6
小小的大肥 <11911839@mail.sustech.edu.cn>
2023-11-30 12:38:30 -0500

    the lob behaviour works well

--- Assets/Scripts/Weapon/WeaponBehaviour.cs
+++ Assets/Scripts/Weapon/WeaponBehaviour.cs
@@ -59,13 +59,8 @@ namespace Weapon
         {
             Pw = pw;
             Pc = pw.gameObject.GetComponent<PlayerController>();
-            
-            // detect player
             Coll.isTrigger = true;
-            
-            
             gameObject.layer = LayerMask.NameToLayer("Player");
-            
             OnNotSelected();
             Rb.constraints = RigidbodyConstraints.FreezeAll;
         }
@@ -77,10 +72,8 @@ namespace Weapon
         {
             Pw = null;
             Pc = null;
-            
             // ignore player collision when drop
             Coll.isTrigger = false;
-            
             gameObject.layer = LayerMask.NameToLayer("Weapon");
             transform.parent = GameObject.Find("[Weapon]").transform;
             Rb.constraints = RigidbodyConstraints.FreezeRotation;
