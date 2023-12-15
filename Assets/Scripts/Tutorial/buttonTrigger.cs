47be148aa3c6d14c7de1be2e0072680535d1ef99
Asad-Anjum <anjumasad967@gmail.com>
2023-12-15 04:51:01 -0500

    reworked tutorial B

--- Assets/Scripts/Tutorial/buttonTrigger.cs
+++ Assets/Scripts/Tutorial/buttonTrigger.cs
@@ -2,29 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace LevelTutorial
+{
 public class buttonTrigger : MonoBehaviour
 {
-    [SerializeField] private AudioSource openSFX;
-    public GameObject door;
-    private bool open = false;
     // Update is called once per frame
     void OnTriggerEnter(Collider col)
     {
 
-        if(col.tag == "Enemy")
+        if(col.tag == "Player Hitbox")
         {
-            openSFX.Play();
-            open = true;
+            LockDoorBehaviour.Instance.OnOpenDoor();
         }
     }
 
-    void Update()
-    {
-        if(open)
-        {
-            Vector3 endPoint = door.transform.eulerAngles;
-            endPoint.y = 0;
-            door.transform.eulerAngles = Vector3.Lerp(door.transform.eulerAngles, endPoint, 0.02f);
-        }
-    }
+}
 }
