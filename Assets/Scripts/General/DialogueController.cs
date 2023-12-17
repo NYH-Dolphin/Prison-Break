fbcfb1ee5bb7b7f1b863ce81ad4851fd87ab26a7
Dolphin <yn2273@nyu.edu>
2023-12-16 20:32:01 -0500

    dialogue controller

---
+++ Assets/Scripts/General/DialogueController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace General
+{
+    public class DialogueController : MonoBehaviour
+    {
+        public List<GameObject> dialogues;
+        public string levelName;
+
+        private bool bDev = false;
+
+        private void Awake()
+        {
+            if (!bDev)
+            {
+                if (PlayerPrefs.GetInt(levelName) == 1)
+                {
+                    foreach (var obj in dialogues)
+                    {
+                        obj.SetActive(false);
+                    }
+                }
+                else
+                {
+                    PlayerPrefs.SetInt(levelName, 1);
+                }
+            }
+        }
+    }
+}
