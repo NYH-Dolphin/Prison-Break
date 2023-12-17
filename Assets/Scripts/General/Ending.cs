7a289565229c31d3c8911987ba91499c4507cf32
Dolphin <yn2273@nyu.edu>
2023-12-17 02:01:28 -0500

    finish all

---
+++ Assets/Scripts/General/Ending.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+namespace General
+{
+    public class Ending : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer video;
+
+        private void Update()
+        {
+            if (!video.isPlaying)
+            {
+                SceneManager.LoadScene("Opening Scene");
+            }
+
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("Opening Scene");
+            }
+        }
+    }
+}
